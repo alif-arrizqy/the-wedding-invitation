2893882e3a111db537f4d36d55870d6f9311cbe7
alif-arrizqy <alif.arrizqy20@gmail.com>
2025-02-11 15:26:28 +0700

    update: remove gift

--- database/seeders/GiftSeeder.php
+++ database/seeders/GiftSeeder.php
@@ -16,14 +16,6 @@ class GiftSeeder extends Seeder
     {
         Gift::create([
             'wedding_id'        => 1,
-            'name'              => 'Rumah Alif',
-            'address'           => 'Kp. Babakan Ds. Dayeuh No. 243 RT 02 RW 05 Kec. Cileungsi Kab. Bogor',
-            'maps'              => 'https://maps.app.goo.gl/T1do7S6moP88TSQo8',
-            'created_at'        => now(),
-            'updated_at'        => now()
-        ]);
-        Gift::create([
-            'wedding_id'        => 1,
             'name'              => 'Rumah Pika',
             'address'           => 'Griya Pratama Mas Blok A No. 3 RT 02 RW 05 Kec. Setu Kab. Bekasi',
             'maps'              => 'https://maps.app.goo.gl/RqsdRkRFTfjXmeBx6',
