3e7f71fafcc0d71cbc888a0a6ce47e7b37f02f64
alif-arrizqy <alif.arrizqy20@gmail.com>
2025-02-28 15:28:46 +0700

    update: change address

--- database/seeders/GiftSeeder.php
+++ database/seeders/GiftSeeder.php
@@ -17,7 +17,7 @@ class GiftSeeder extends Seeder
         Gift::create([
             'wedding_id'        => 1,
             'name'              => 'Rumah Pika',
-            'address'           => 'Griya Pratama Mas Blok A No. 3 RT 02 RW 05 Kec. Setu Kab. Bekasi',
+            'address'           => 'Griya Pratama Mas Blok B4 No.02 RT 004 RW 007 Desa Cikarageman Kec. Setu Kab. Bekasi',
             'maps'              => 'https://maps.app.goo.gl/RqsdRkRFTfjXmeBx6',
             'created_at'        => now(),
             'updated_at'        => now()
