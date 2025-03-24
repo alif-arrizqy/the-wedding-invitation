68a5176d9dfb10cc10d7c403de836ed586f20860
alif-arrizqy <alif.arrizqy20@gmail.com>
2025-03-24 11:28:42 +0700

    feat: add ecosystem configuration for Laravel and Vite development

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [{
+    name: "laravel-serve",
+    script: "php",
+    args: "artisan serve",
+    cwd: "/home/pi/the-wedding-invitation",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+  }, {
+    name: "vite-dev",
+    script: "npm",
+    args: "run dev",
+    cwd: "/home/pi/the-wedding-invitation",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    },
+  ],
+};
