b23d5701a60549e60b0ab7f7ca2b20cd5a08bbe9
alif-arrizqy <alif.arrizqy20@gmail.com>
2025-04-10 11:50:55 +0700

    feat: generate url invitation

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -4,11 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 
 class Guest extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    protected $fillable = [
+        'wedding_id',
+        'name',
+        'isVIP',
+        'url',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($guest) {
+            if (empty($guest->url)) {
+                // Generate URL following the seeder pattern
+                $slug = strtolower(str_replace([' ', '&'], ['-', 'and'], $guest->name));
+                $guest->url = "https://wedding-invitation.com/?to={$slug}";
+            }
+        });
+    }
 
     public function Wedding()
     {
