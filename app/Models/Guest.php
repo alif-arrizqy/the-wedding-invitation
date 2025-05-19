ad2b8640d782624581ea4666d1667c919dc982c8
alif-arrizqy <alif.arrizqy20@gmail.com>
2025-05-19 10:42:13 +0700

    update: refactor Guest model to use booted method and ensure URL generation on create and update

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -5,34 +5,47 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Guest extends Model
 {
     use HasFactory;
-    protected $guarded = [];
 
     protected $fillable = [
         'wedding_id',
         'name',
         'isVIP',
+        'jenis_tamu',
+        'is_sent',
         'url',
     ];
 
-    protected static function boot()
+    protected static function booted()
     {
-        parent::boot();
-
         static::creating(function ($guest) {
-            if (empty($guest->url)) {
-                // Generate URL following the seeder pattern
-                $slug = strtolower(str_replace([' ', '&'], ['-', 'and'], $guest->name));
-                $guest->url = "https://momen-bahagia.site/?to={$slug}";
+            // Don't set URL on create - wait for ID to be assigned
+        });
+
+        static::created(function ($guest) {
+            // Generate URL with the new format
+            $guestSlug = str_replace(' ', '-', strtolower($guest->name));
+            $guest->url = "https://momen-bahagia.site/?to=" . urlencode($guestSlug);
+            $guest->saveQuietly(); // Save without triggering events again
+        });
+
+        static::updating(function ($guest) {
+            // Only update URL if the name has changed
+            if ($guest->isDirty('name')) {
+                $guestSlug = str_replace(' ', '-', strtolower($guest->name));
+                $guest->url = "https://momen-bahagia.site/?to=" . urlencode($guestSlug);
             }
         });
     }
 
-    public function Wedding()
+    public function wedding(): BelongsTo
     {
         return $this->belongsTo(Wedding::class);
     }
 }
+
+// $guest->url = "https://momen-bahagia.site/?to={$slug}";
